fd32b80c3dfb85e052c22cdfd468721f6adb118d
strixsoul <rsilveira136@gmail.com>
2021-02-16 00:59:59 -0300

    created project to access sites with puppeteer

---
+++ index.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
+
+console.log('Bem vindo');
+
+async function robo() {
+    const browser = await puppeteer.launch({ headless: true });
+    
+    try {
+        const page = await browser.newPage();
+        const baseCurency = readlineSync.question('Informe uma moeda base: ') || 'dolar';
+        const finalCurency = readlineSync.question('Informe uma moeda final: ') || 'real';
+        const someUrl = `https://www.google.com/search?q=${baseCurency}+para+${finalCurency}&oq=${baseCurency}+para+${finalCurency}&aqs=chrome..69i57j0j0i10i433j0l5.3016j1j7&sourceid=chrome&ie=UTF-8`;
+        await page.goto(someUrl);
+        // await page.screenshot({ path: 'example.png' });
+    
+        const resultado = await page.evaluate(() => {
+            return document.querySelector('.a61j6.vk_gy.vk_sh.Hg3mWc').value;
+        });
+    
+        console.log(`O valor de 1 ${baseCurency} em ${finalCurency} é ${resultado}`);
+    } catch (error) {
+        console.log("Não foi possível encontrar o resultado, verifique se a ortografia está correta e tente novamente.")
+    } finally {
+        await browser.close();
+    }
+}
+
+robo();
